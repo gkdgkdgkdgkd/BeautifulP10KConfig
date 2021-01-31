702a93f1f0c4c399ecfce9965eb86ff09b0b4128
2293736867 <2293736867@qq.com>
2021-01-30 21:28:23 -0500

    update 01/30/2021

--- script/p10kgui.zsh
+++ script/p10kgui.zsh
@@ -1,3 +1,6 @@
+#   show colors
+#   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+
 'builtin' 'local' '-a' 'p10k_config_opts'
 [[ ! -o 'aliases'         ]] || p10k_config_opts+=('aliases')
 [[ ! -o 'sh_glob'         ]] || p10k_config_opts+=('sh_glob')
@@ -11,27 +14,41 @@
 
   autoload -Uz is-at-least && is-at-least 5.1 || return
 
+  # Left prompt
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+    # Line 1
     date
+    time
+    newline
+
+    # Line 2
+    ip
     wifi
     newline
 
+    # Line 3
     dir                     
     vcs                     
     prompt_char           
   )
 
+  # Right prompt
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+    # Line 1
     ram
     swap
-    disk_usage
     load
     battery
     newline
 
-    status                  
+    # Line 2
+    proxy
+    public_ip
+    newline
+
+    # Line 3
     command_execution_time  
-    time                    
+    status                  
   )
 
   typeset -g POWERLEVEL9K_MODE=nerdfont-complete
@@ -44,7 +61,6 @@
   typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX='%244F╭─'
   typeset -g POWERLEVEL9K_MULTILINE_NEWLINE_PROMPT_PREFIX='%244F├─'
   typeset -g POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX='%244F╰─'
-
   typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_SUFFIX='%244F─╮'
   typeset -g POWERLEVEL9K_MULTILINE_NEWLINE_PROMPT_SUFFIX='%244F─┤'
   typeset -g POWERLEVEL9K_MULTILINE_LAST_PROMPT_SUFFIX='%244F─╯'
@@ -58,17 +74,17 @@
     typeset -g POWERLEVEL9K_EMPTY_LINE_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL='%{%}'
   fi
 
-  typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='\uE0B5'
-  typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='\uE0B7'
-  typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B4'
-  typeset -g POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR='\uE0B6'
-  typeset -g POWERLEVEL9K_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL='\uE0B4'
-  typeset -g POWERLEVEL9K_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL='\uE0B6'
-  typeset -g POWERLEVEL9K_LEFT_PROMPT_FIRST_SEGMENT_START_SYMBOL='\uE0B6'
-  typeset -g POWERLEVEL9K_RIGHT_PROMPT_LAST_SEGMENT_END_SYMBOL='\uE0B4'
+  typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='\uE0B1'
+  typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='\uE0B3'
+  typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B0'
+  typeset -g POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR='\uE0B2'
+  typeset -g POWERLEVEL9K_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL='\uE0B0'
+  typeset -g POWERLEVEL9K_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL='\uE0B2'
+  typeset -g POWERLEVEL9K_LEFT_PROMPT_FIRST_SEGMENT_START_SYMBOL='\uE0B2'
+  typeset -g POWERLEVEL9K_RIGHT_PROMPT_LAST_SEGMENT_END_SYMBOL='\uE0B0'
   typeset -g POWERLEVEL9K_EMPTY_LINE_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=
 
-  #prompt_char
+  ################################[ prompt_char ]################################
   typeset -g POWERLEVEL9K_PROMPT_CHAR_BACKGROUND=
   typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=76
   typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=196
@@ -81,16 +97,15 @@
   typeset -g POWERLEVEL9K_PROMPT_CHAR_LEFT_PROMPT_FIRST_SEGMENT_START_SYMBOL=
   typeset -g POWERLEVEL9K_PROMPT_CHAR_LEFT_{LEFT,RIGHT}_WHITESPACE=
 
-  #dir
-  typeset -g POWERLEVEL9K_DIR_HOME_BACKGROUND=166
-  typeset -g POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND=166
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=11
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=232
+
+  ##################################[ dir ]##################################
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=202
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=0
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=0
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=0
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
-
   local anchor_files=(
     .bzr
     .citc
@@ -118,13 +133,20 @@
   typeset -g POWERLEVEL9K_SHORTEN_FOLDER_MARKER="(${(j:|:)anchor_files})"
   typeset -g POWERLEVEL9K_DIR_TRUNCATE_BEFORE_MARKER=false
   typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-  typeset -g POWERLEVEL9K_DIR_MAX_LENGTH=80
+  typeset -g POWERLEVEL9K_DIR_MAX_LENGTH=50
   typeset -g POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS=40
   typeset -g POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS_PCT=50
-  typeset -g POWERLEVEL9K_DIR_HYPERLINK=false
-  typeset -g POWERLEVEL9K_DIR_SHOW_WRITABLE=v2
+  typeset -g POWERLEVEL9K_DIR_HYPERLINK=true
 
-  #vcs
+  typeset -g POWERLEVEL9K_DIR_SHOW_WRITABLE=v3
+
+  #####################################[ vcs ]######################################
+  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=47
+  typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=214
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
+  typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=9
+  typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
+
   typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
 
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
@@ -144,23 +166,26 @@
     local conflicted='%1F'
 
     local res
-    local where  
+
     if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}"
-      where=${(V)VCS_STATUS_LOCAL_BRANCH}
-    elif [[ -n $VCS_STATUS_TAG ]]; then
-      res+="${meta}#"
-      where=${(V)VCS_STATUS_TAG}
+      local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
+      (( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
+      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
     fi
 
-    (( $#where > 32 )) && where[13,-13]="…"
+    if [[ -n $VCS_STATUS_TAG
+          && -z $VCS_STATUS_LOCAL_BRANCH  # <-- this line
+        ]]; then
+      local tag=${(V)VCS_STATUS_TAG}
+      (( $#tag > 32 )) && tag[13,-13]="…"  # <-- this line
+      res+="${meta}#${clean}${tag//\%/%%}"
+    fi
 
-    res+="${clean}${where//\%/%%}"  
-
-    [[ -z $where ]] && res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
+    [[ -z $VCS_STATUS_LOCAL_BRANCH && -z $VCS_STATUS_LOCAL_BRANCH ]] &&  # <-- this line
+      res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
 
     if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
-      res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"  # escape %
+      res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"
     fi
 
     (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
@@ -189,11 +214,11 @@
   typeset -g POWERLEVEL9K_VCS_CONTENT_EXPANSION='${$((my_git_formatter()))+${my_git_format}}'
   typeset -g POWERLEVEL9K_VCS_{STAGED,UNSTAGED,UNTRACKED,CONFLICTED,COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=-1
 
-  typeset -g POWERLEVEL9K_VCS_PREFIX='on '
+  typeset -g POWERLEVEL9K_VCS_PREFIX='Git '
 
   typeset -g POWERLEVEL9K_VCS_BACKENDS=(git)
 
-  #status
+  ##########################[ status ]###########################
   typeset -g POWERLEVEL9K_STATUS_EXTENDED_STATES=true
 
   typeset -g POWERLEVEL9K_STATUS_OK=true
@@ -222,59 +247,74 @@
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_FOREGROUND=0
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_BACKGROUND=196
 
-  #command_execution_time
+  ###################[ command_execution_time ]###################
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=0
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_BACKGROUND=3
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_BACKGROUND=226
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=2
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FORMAT='d h m s'
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='took '
 
-
-  #disk_usage
-  typeset -g POWERLEVEL9K_DISK_USAGE_FOREGROUND=0
-  typeset -g POWERLEVEL9K_DISK_USAGE_NORMAL_BACKGROUND=47
-  typeset -g POWERLEVEL9K_DISK_USAGE_WARNING_BACKGROUND=160
-  typeset -g POWERLEVEL9K_DISK_USAGE_CRITICAL_BACKGROUND=196
-  typeset -g POWERLEVEL9K_DISK_USAGE_WARNING_LEVEL=90
-  typeset -g POWERLEVEL9K_DISK_USAGE_CRITICAL_LEVEL=95
-  typeset -g POWERLEVEL9K_DISK_USAGE_ONLY_WARNING=false
-
-  #ram
+  ######################################[ ram ]#######################################
+  typeset -g POWERLEVEL9K_RAM_FOREGROUND=0
   typeset -g POWERLEVEL9K_RAM_BACKGROUND=47
 
-  #swap
+  #####################################[ swap ]######################################
   typeset -g POWERLEVEL9K_SWAP_FOREGROUND=0
   typeset -g POWERLEVEL9K_SWAP_BACKGROUND=47
 
-  #cpu load
+  ######################################[ load ]######################################
   typeset -g POWERLEVEL9K_LOAD_WHICH=1
+  typeset -g POWERLEVEL9K_LOAD_NORMAL_FOREGROUND=0
   typeset -g POWERLEVEL9K_LOAD_NORMAL_BACKGROUND=47
+  typeset -g POWERLEVEL9K_LOAD_WARNING_FOREGROUND=0
   typeset -g POWERLEVEL9K_LOAD_WARNING_BACKGROUND=160
+  typeset -g POWERLEVEL9K_LOAD_CRITICAL_FOREGROUND=0
   typeset -g POWERLEVEL9K_LOAD_CRITICAL_BACKGROUND=196
 
-  #battery
+  ###############################[ public_ip ]###############################
+  typeset -g POWERLEVEL9K_PUBLIC_IP_FOREGROUND=0
+  typeset -g POWERLEVEL9K_PUBLIC_IP_BACKGROUND=48
+
+  ###########[ ip ]###########
+  typeset -g POWERLEVEL9K_IP_FOREGROUND=0
+  typeset -g POWERLEVEL9K_IP_BACKGROUND=48
+  typeset -g POWERLEVEL9K_IP_CONTENT_EXPANSION='$P9K_IP_INTERFACE ${P9K_IP_RX_RATE:+⇣$P9K_IP_RX_RATE }${P9K_IP_TX_RATE:+⇡$P9K_IP_TX_RATE }$P9K_IP_IP'
+  typeset -g POWERLEVEL9K_IP_INTERFACE='[ew].*'
+  typeset -g POWERLEVEL9K_IP_VISUAL_IDENTIFIER_EXPANSION=''
+
+  #########################[ proxy ]##########################
+  typeset -g POWERLEVEL9K_PROXY_FOREGROUND=0
+  typeset -g POWERLEVEL9K_PROXY_BACKGROUND=205
+  typeset -g POWERLEVEL9K_PROXY_VISUAL_IDENTIFIER_EXPANSION='爵'
+
+  ################################[ battery ]#################################
   typeset -g POWERLEVEL9K_BATTERY_FOREGROUND=0
   typeset -g POWERLEVEL9K_BATTERY_LOW_THRESHOLD=20
-  typeset -g POWERLEVEL9K_BATTERY_LOW_BACKGROUND=1
+  typeset -g POWERLEVEL9K_BATTERY_LOW_BACKGROUND=196
   typeset -g POWERLEVEL9K_BATTERY_{CHARGING,CHARGED}_BACKGROUND=47
-  typeset -g POWERLEVEL9K_BATTERY_DISCONNECTED_FOREGROUND=214
+  typeset -g POWERLEVEL9K_BATTERY_DISCONNECTED_BACKGROUND=214
   typeset -g POWERLEVEL9K_BATTERY_STAGES='\uf58d\uf579\uf57a\uf57b\uf57c\uf57d\uf57e\uf57f\uf580\uf581\uf578'
   typeset -g POWERLEVEL9K_BATTERY_VERBOSE=false
 
-  #wifi
-  typeset -g POWERLEVEL9K_WIFI_BACKGROUND=47
+  #####################################[ wifi ]#####################################
+  typeset -g POWERLEVEL9K_WIFI_FOREGROUND=0
+  typeset -g POWERLEVEL9K_WIFI_BACKGROUND=48
 
-  #date and time
+  typeset -g my_wifi_icon=('睊 Very Low...' '睊 Low...' '  Medium' '  Well!' '  Nice!')     
+  typeset -g POWERLEVEL9K_WIFI_VISUAL_IDENTIFIER_EXPANSION=' $P9K_WIFI_SSID   ${my_wifi_icon[P9K_WIFI_BARS+1]}'
+
+  ####################################[ date and time ]####################################
   typeset -g POWERLEVEL9K_DATE_FORMAT='%D{%Y-%m-%d}'
+  typeset -g POWERLEVEL9K_DATE_FOREGROUND=0
   typeset -g POWERLEVEL9K_DATE_BACKGROUND=231
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=0
   typeset -g POWERLEVEL9K_TIME_BACKGROUND=231
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=false
 
   typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=always
-
-  typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+  typeset -g POWERLEVEL9K_INSTANT_PROMPT=verbose
 
   typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
 
--- script/p10ktty.zsh
+++ script/p10ktty.zsh
@@ -6,42 +6,49 @@
 
 () {
   emulate -L zsh -o extended_glob
-
   unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
 
   autoload -Uz is-at-least && is-at-least 5.1 || return
 
+  # Left prompt
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    date 
-    wifi
-    newline                 
-
+    # Line 1
+    date
     time
-    battery
     newline
 
-    dir
-    vcs
+    # Line 2
+    ip 
+    wifi
+    newline
+
+    # Line 3
+    dir                    
+    vcs                     
     prompt_char             
   )
 
+  # Right prompt
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+    # Line 1
     ram
     swap
-    newline
-
-    disk_usage
     load
+    battery
     newline
 
-    status                  
-    command_execution_time 
+    # Line 2
+    proxy
+    public_ip
+    newline
+
+    # Line 3
+    command_execution_time
+    status
   )
 
   typeset -g POWERLEVEL9K_MODE=ascii
   typeset -g POWERLEVEL9K_ICON_PADDING=none
-  typeset -g POWERLEVEL9K_DATE_FOREGROUND=6
-
 
   typeset -g POWERLEVEL9K_BACKGROUND=                            
   typeset -g POWERLEVEL9K_{LEFT,RIGHT}_{LEFT,RIGHT}_WHITESPACE=  
@@ -49,7 +56,6 @@
   typeset -g POWERLEVEL9K_{LEFT,RIGHT}_SEGMENT_SEPARATOR=        
 
   typeset -g POWERLEVEL9K_ICON_BEFORE_CONTENT=true
-
   typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
 
   typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=
@@ -58,7 +64,6 @@
   typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_SUFFIX=
   typeset -g POWERLEVEL9K_MULTILINE_NEWLINE_PROMPT_SUFFIX=
   typeset -g POWERLEVEL9K_MULTILINE_LAST_PROMPT_SUFFIX=
-
   typeset -g POWERLEVEL9K_LEFT_PROMPT_FIRST_SEGMENT_START_SYMBOL=
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_LAST_SEGMENT_END_SYMBOL=
 
@@ -66,7 +71,7 @@
   typeset -g POWERLEVEL9K_RULER_CHAR='-'        
   typeset -g POWERLEVEL9K_RULER_FOREGROUND=7
 
-  typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR='-'
+  typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR='.'
   if [[ $POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR != ' ' ]]; then
     typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_FOREGROUND=7
     typeset -g POWERLEVEL9K_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=' '
@@ -75,23 +80,21 @@
     typeset -g POWERLEVEL9K_EMPTY_LINE_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL='%{%}'
   fi
 
-  #prompt_char
+  ################################[ prompt_char ]################################
   typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=2
   typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=1
   typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='>'
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='-'
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='*'
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIOWR_CONTENT_EXPANSION='!'
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='<'
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='V'
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIOWR_CONTENT_EXPANSION='^'
   typeset -g POWERLEVEL9K_PROMPT_CHAR_OVERWRITE_STATE=true
   typeset -g POWERLEVEL9K_PROMPT_CHAR_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=''
   typeset -g POWERLEVEL9K_PROMPT_CHAR_LEFT_PROMPT_FIRST_SEGMENT_START_SYMBOL=
 
-  #dir
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=7
+  ##################################[ dir ]##################################
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=3
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=3
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=3
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=false
   local anchor_files=(
     .bzr
@@ -123,11 +126,15 @@
   typeset -g POWERLEVEL9K_DIR_MAX_LENGTH=80
   typeset -g POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS=40
   typeset -g POWERLEVEL9K_DIR_MIN_COMMAND_COLUMNS_PCT=50
-  typeset -g POWERLEVEL9K_DIR_HYPERLINK=false
-  typeset -g POWERLEVEL9K_DIR_SHOW_WRITABLE=v2
+  typeset -g POWERLEVEL9K_DIR_HYPERLINK=true
 
+  typeset -g POWERLEVEL9K_DIR_SHOW_WRITABLE=v3
 
-  #vcs
+  typeset -g POWERLEVEL9K_DIR_CLASSES=()
+
+  #####################################[ vcs ]######################################
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
+
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
 
   function my_git_formatter() {
@@ -139,37 +146,40 @@
     fi
 
     if (( $1 )); then
-      local       meta='%f'   
-      local      clean='%2F'  
-      local   modified='%3F'  
-      local  untracked='%4F'  
-      local conflicted='%1F'  
+      local       meta='%f'   # default foreground
+      local      clean='%2F'  # green foreground
+      local   modified='%3F'  # yellow foreground
+      local  untracked='%4F'  # blue foreground
+      local conflicted='%1F'  # red foreground
     else
-      local       meta='%f'  
-      local      clean='%f'  
-      local   modified='%f'  
-      local  untracked='%f'  
-      local conflicted='%f'  
+      local       meta='%f'  # default foreground
+      local      clean='%f'  # default foreground
+      local   modified='%f'  # default foreground
+      local  untracked='%f'  # default foreground
+      local conflicted='%f'  # default foreground
     fi
 
     local res
-    local where  # branch or tag
+
     if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}"
-      where=${(V)VCS_STATUS_LOCAL_BRANCH}
-    elif [[ -n $VCS_STATUS_TAG ]]; then
-      res+="${meta}#"
-      where=${(V)VCS_STATUS_TAG}
+      local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
+      (( $#branch > 32 )) && branch[13,-13]=".."  # <-- this line
+      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
     fi
 
-    (( $#where > 32 )) && where[13,-13]=".."
+    if [[ -n $VCS_STATUS_TAG
+          && -z $VCS_STATUS_LOCAL_BRANCH  # <-- this line
+        ]]; then
+      local tag=${(V)VCS_STATUS_TAG}
+      (( $#tag > 32 )) && tag[13,-13]=".."  # <-- this line
+      res+="${meta}#${clean}${tag//\%/%%}"
+    fi
 
-    res+="${clean}${where//\%/%%}"  # escape %
-
-    [[ -z $where ]] && res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
+    [[ -z $VCS_STATUS_LOCAL_BRANCH && -z $VCS_STATUS_LOCAL_BRANCH ]] &&  # <-- this line
+      res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
 
     if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
-      res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"  # escape %
+      res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"
     fi
 
     (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}<${VCS_STATUS_COMMITS_BEHIND}"
@@ -191,6 +201,7 @@
   functions -M my_git_formatter 2>/dev/null
 
   typeset -g POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY=-1
+
   typeset -g POWERLEVEL9K_VCS_DISABLED_WORKDIR_PATTERN='~'
 
   typeset -g POWERLEVEL9K_VCS_DISABLE_GITSTATUS_FORMATTING=true
@@ -201,18 +212,17 @@
   typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_COLOR=2
   typeset -g POWERLEVEL9K_VCS_LOADING_VISUAL_IDENTIFIER_COLOR=
   typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_EXPANSION=
-  typeset -g POWERLEVEL9K_VCS_PREFIX='on '
+  typeset -g POWERLEVEL9K_VCS_PREFIX='%fGit '
 
   typeset -g POWERLEVEL9K_VCS_BACKENDS=(git)
-
   typeset -g POWERLEVEL9K_VCS_CLEAN_FOREGROUND=2
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_FOREGROUND=2
   typeset -g POWERLEVEL9K_VCS_MODIFIED_FOREGROUND=3
 
-  #status
+  ##########################[ status ]###########################
   typeset -g POWERLEVEL9K_STATUS_EXTENDED_STATES=true
 
-  typeset -g POWERLEVEL9K_STATUS_OK=false
+  typeset -g POWERLEVEL9K_STATUS_OK=true
   typeset -g POWERLEVEL9K_STATUS_OK_FOREGROUND=2
   typeset -g POWERLEVEL9K_STATUS_OK_VISUAL_IDENTIFIER_EXPANSION='ok'
 
@@ -220,48 +230,50 @@
   typeset -g POWERLEVEL9K_STATUS_OK_PIPE_FOREGROUND=2
   typeset -g POWERLEVEL9K_STATUS_OK_PIPE_VISUAL_IDENTIFIER_EXPANSION='ok'
 
-  typeset -g POWERLEVEL9K_STATUS_ERROR=false
+  typeset -g POWERLEVEL9K_STATUS_ERROR=true
   typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND=1
   typeset -g POWERLEVEL9K_STATUS_ERROR_VISUAL_IDENTIFIER_EXPANSION='err'
 
   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL=true
   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND=1
   typeset -g POWERLEVEL9K_STATUS_VERBOSE_SIGNAME=false
-  typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION=
+  typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION='err'
 
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE=true
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_FOREGROUND=1
   typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_VISUAL_IDENTIFIER_EXPANSION='err'
 
-  #command_execution_time
+  ###################[ command_execution_time ]###################
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=3
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FORMAT='d h m s'
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_VISUAL_IDENTIFIER_EXPANSION=
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='took '
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PREFIX='%ftook '
 
-  #disk_usage
-  typeset -g POWERLEVEL9K_DISK_USAGE_NORMAL_FOREGROUND=2
-  typeset -g POWERLEVEL9K_DISK_USAGE_WARNING_FOREGROUND=3
-  typeset -g POWERLEVEL9K_DISK_USAGE_CRITICAL_FOREGROUND=1
-  typeset -g POWERLEVEL9K_DISK_USAGE_WARNING_LEVEL=90
-  typeset -g POWERLEVEL9K_DISK_USAGE_CRITICAL_LEVEL=95
-  typeset -g POWERLEVEL9K_DISK_USAGE_ONLY_WARNING=false
-
-  #ram
+  ######################################[ ram ]#######################################
   typeset -g POWERLEVEL9K_RAM_FOREGROUND=2
 
-  #swap
+  #####################################[ swap ]######################################
   typeset -g POWERLEVEL9K_SWAP_FOREGROUND=2
 
-  #load
+  ######################################[ load ]######################################
   typeset -g POWERLEVEL9K_LOAD_WHICH=1
   typeset -g POWERLEVEL9K_LOAD_NORMAL_FOREGROUND=2
   typeset -g POWERLEVEL9K_LOAD_WARNING_FOREGROUND=3
   typeset -g POWERLEVEL9K_LOAD_CRITICAL_FOREGROUND=1
 
-  #battery
+  ###############################[ public_ip ]###############################
+  typeset -g POWERLEVEL9K_PUBLIC_IP_FOREGROUND=2
+
+  ###########[ ip ]###########
+  typeset -g POWERLEVEL9K_IP_FOREGROUND=2
+  typeset -g POWERLEVEL9K_IP_CONTENT_EXPANSION='$P9K_IP_INTERFACE $P9K_IP_IP${P9K_IP_RX_RATE:+ %2F<$P9K_IP_RX_RATE}${P9K_IP_TX_RATE:+ %3F>$P9K_IP_TX_RATE}'
+  typeset -g POWERLEVEL9K_IP_INTERFACE='[ew].*'
+
+  #########################[ proxy ]##########################
+  typeset -g POWERLEVEL9K_PROXY_FOREGROUND=5
+
+  ################################[ battery ]#################################
   typeset -g POWERLEVEL9K_BATTERY_LOW_THRESHOLD=20
   typeset -g POWERLEVEL9K_BATTERY_LOW_FOREGROUND=1
   typeset -g POWERLEVEL9K_BATTERY_{CHARGING,CHARGED}_FOREGROUND=2
@@ -269,26 +281,26 @@
   typeset -g POWERLEVEL9K_BATTERY_STAGES=('battery')
   typeset -g POWERLEVEL9K_BATTERY_VERBOSE=false
 
-  #wifi
+  #####################################[ wifi ]#####################################
   typeset -g POWERLEVEL9K_WIFI_FOREGROUND=2
+  typeset -g my_wifi_icon=('Very Low...' 'Low...' 'Medium' 'Well!' 'Nice!')
+  typeset -g POWERLEVEL9K_WIFI_VISUAL_IDENTIFIER_EXPANSION='<$P9K_WIFI_SSID> ${my_wifi_icon[P9K_WIFI_BARS+1]}'
 
-  #date and time
+  ####################################[ date and time ]####################################
   typeset -g POWERLEVEL9K_DATE_FORMAT='%D{%Y-%m-%d}'
   typeset -g POWERLEVEL9K_DATE_FOREGROUND=7
   typeset -g POWERLEVEL9K_TIME_FOREGROUND=7
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=false
+  typeset -g POWERLEVEL9K_TIME_VISUAL_IDENTIFIER_EXPANSION=
+  typeset -g POWERLEVEL9K_TIME_PREFIX='%fat '
 
   typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=always
-
-  typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-
+  typeset -g POWERLEVEL9K_INSTANT_PROMPT=verbose
   typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
-
   (( ! $+functions[p10k] )) || p10k reload
 }
 
-# Tell `p10k configure` which file it should overwrite.
 typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
